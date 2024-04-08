0cc6973bdf171b2cfec2b5ab3a266dfcb6445fb8
Schorchi <schorchi@gmail.com>
2024-04-08 13:22:01 -0300

    corrected background image and tons of typescript errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
+import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+
 
 test("renders learn react link", () => {
   render(<App />);
