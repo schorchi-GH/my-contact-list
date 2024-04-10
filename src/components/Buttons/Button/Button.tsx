9a0551aa41ac22ad8f8f351f4c7ce52f122d35c8
Schorchi <schorchi@gmail.com>
2024-04-10 15:26:00 -0300

    aletrações em button styled components

--- src/components/Buttons/Button/Button.tsx
+++ src/components/Buttons/Button/Button.tsx
@@ -1,20 +1,37 @@
-import SvgIcon from "@mui/material/SvgIcon";
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
-import "./Button.scss";
 
 interface ButtonProps {
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
   isIcon?: boolean;
-  value: string;
-  children: string;
+  value?: "Edit" | "Save" | "Login";
+  children: ReactNode;
 }
 
+interface SvgIconProps {
+  component: React.ElementType;
+  className?: string;
+}
+
+const StyledSvgIcon = styled(({ component: Component, className }: SvgIconProps) => (
+  <Component className={className} />
+))`
+  width: 17px;
+  height: 17px;
+  margin-left: 5px;
+`;
+
 const ButtonIcon = {
-  Edit: <EditOutlinedIcon />,
-  Save: <SaveOutlinedIcon />,
+  Edit: EditOutlinedIcon,
+  Save: SaveOutlinedIcon,
 };
+
+const StyledButton = styled.button`
+    // Estilos omitidos para brevidade
+`;
 
 export const Button = ({
   onClick,
@@ -23,8 +40,10 @@ export const Button = ({
   type = "button",
   isIcon,
 }: ButtonProps) => (
-  <button className="button" onClick={onClick} value={value} type={type}>
+  <StyledButton onClick={onClick} value={value} type={type}>
     {children}
-    {isIcon && <SvgIcon className="button-icon">{ButtonIcon[value as keyof typeof ButtonIcon]}</SvgIcon>}
-  </button>
+    {isIcon && value && Object.hasOwnProperty.call(ButtonIcon, value) ? (
+      <StyledSvgIcon component={ButtonIcon[value as keyof typeof ButtonIcon]} />
+    ) : null}
+  </StyledButton>
 );
